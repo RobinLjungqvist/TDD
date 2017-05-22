977035e186622157d3ad0efe8609fdaa6e5d83cd
Roppin.L <robin.ljungqvist@gmail.com>
2017-05-22 09:53:14 +0200

    Exercise1

---
+++ Exercise1/Exercise1/Validator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using ValidationEngine.Exceptions;
+
+namespace ValidationEngine
+{
+    public class Validator
+    {
+        public Validator()
+        {
+        }
+
+        public bool ValidateEmailAdress(string email)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            var match = regex.Match(email);
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                throw new InvalidEmailException();
+            }
+        
+        }
+    }
+}
---
+++ Exercise1/ValidationEngine.Tests/ValidationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationEngine.Exceptions;
+
+namespace ValidationEngine.Tests
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+        [Test]
+        public void TrueForValidAddress()
+        {
+            var sut = new Validator();
+
+            var result = sut.ValidateEmailAdress("nisse@email.com");
+
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void FalseForInvalidAddress()
+        {
+            var sut = new Validator();
+
+            Assert.Throws<InvalidEmailException>(() =>
+            {
+                var result = sut.ValidateEmailAdress("Det här är ingen email.");
+                var result2 = sut.ValidateEmailAdress("felemail.com");
+
+            });
+        }
+    }
+}
